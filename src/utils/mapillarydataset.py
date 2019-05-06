3c6d47d35eff235fd74e316fb2fe9717a5a7a5ce
Yvan Satyawan <y_satyawan@hotmail.com>
2019-05-06 16:51:16 +0200

    Dataset now also resizes all images to the same size

--- src/utils/mapillarydataset.py
+++ src/utils/mapillarydataset.py
@@ -8,64 +8,100 @@ containing the raw image, the segmentation, and the panoptic segmentation.
 Authors:
     Yvan Satyawan <y_satyawan@hotmail.com>
 """
-import torch
-from os import listdir
-from os.path import join, splitext
-
-from torch.utils.data import Dataset
-from skimage import io
-import imgaug as ia
-from imgaug import augmenters as iaa
 import numpy as np
+import torch
+from torch.utils.data import Dataset
+from imgaug import augmenters as iaa
+from os.path import join
 from PIL import Image
+from skimage import io
 
 
-# Here we define augmentation
-class Augment:
-    @staticmethod
-    def augment(image):
-        """Augments an image in the dataset.
+DIM_WIDTH = 420
+DIMENSIONS = (DIM_WIDTH, int(float(DIM_WIDTH) * .75))
 
-        Args:
-            image (imageio.core.util.Array): The image to be augmented.
 
-        Returns:
-            imageio.core.util.Array: The augmented image.
-        """
+def augment(image):
+    """Augments a given input image.
 
-        # Define the probabilities
-        def rl(aug):
-            """Defines the "rarely" probability value."""
-            return iaa.Sometimes(0.09, aug)
+    Args:
+        image (imageio.core.util.Array): The image to be augmented.
 
-        def oc(aug):
-            """Defines the "occasionally" probability value."""
-            return iaa.Sometimes(0.3, aug)
+    Returns:
+        imageio.core.util.Array: The augmented image.
+    """
 
-        def st(aug):
-            """Defines the "sometimes" probability value."""
-            return iaa.Sometimes(0.4, aug)
+    # Define the probabilities
+    def rl(aug):
+        """Defines the "rarely" probability value."""
+        return iaa.Sometimes(0.09, aug)
 
-        # set up the sequential augmentation
-        seq = iaa.Sequential([
-            # blur images with a sigma between 0 and 1.5
-            rl(iaa.GaussianBlur((0, 1.5))),
-            # randomly remove up to X% of the pixels
-            oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)),
-            # randomly remove up to X% of the pixels
-            oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2),
-                                 per_channel=0.5)),
-            # change brightness of images (by -X to Y of original value)
-            oc(iaa.Add((-40, 40), per_channel=0.5)),
-            # change brightness of images (X-Y% of original value)
-            st(iaa.Multiply((0.10, 2.5), per_channel=0.2)),
-            # improve or worsen the contrast
-            rl(iaa.ContrastNormalization((0.5, 1.5), per_channel=0.5)),
-        ],
-            random_order=True)
+    def oc(aug):
+        """Defines the "occasionally" probability value."""
+        return iaa.Sometimes(0.3, aug)
 
-        # Return the augmented image
-        return seq.augment_image(image)
+    def st(aug):
+        """Defines the "sometimes" probability value."""
+        return iaa.Sometimes(0.4, aug)
+
+    # set up the sequential augmentation
+    seq = iaa.Sequential([
+        # Blur images with a sigma between 0 and 1.5
+        rl(iaa.GaussianBlur((0, 1.5))),
+
+        # Pepper X% of the pixels
+        oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)),
+
+        # Pepper X% of the pixels
+        oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2),
+                             per_channel=0.5)),
+
+        # Brightness (by -X to Y of original value)
+        oc(iaa.Add((-40, 40), per_channel=0.5)),
+
+        # Brightness (X-Y% of original value)
+        st(iaa.Multiply((0.10, 2.5), per_channel=0.2)),
+
+        # Contrast
+        rl(iaa.ContrastNormalization((0.5, 1.5), per_channel=0.5)),
+    ],
+        random_order=True)
+
+    # Return the augmented image
+    return seq.augment_image(image)
+
+
+def resize(image, interpolation):
+    """Resizes a given input image to DIMENSIONS
+
+    Args:
+        image (imageio.core.util.Array): The image to be resized.
+        interpolation (str): Sampling when resizing. Either 'cubic' or
+                             'nearest'.
+
+    Returns:
+        imageio.core.util.Array: The resized image.
+    """
+    # Calculate what the cropped size should be based on dimensions
+    if 4 * image.shape[1] >= 3 * image.shape[0]:
+        # the height is the smaller element
+        cropped_height = image.shape[0]
+        cropped_width = int(image.shape[0] * 1.33333333333333)
+    else:
+        cropped_height = int(image.shape[1] * .75)
+        cropped_width = image.shape[1]
+
+    # Define the transformations
+    seq = iaa.Sequential([
+        # Crop to aspect ratio
+        iaa.CropToFixedSize(cropped_width, cropped_height, position='center'),
+
+        # Resize to proper size
+        iaa.Resize({"width": DIMENSIONS[0],
+                    "height": DIMENSIONS[1]}, interpolation=interpolation)
+    ])
+
+    return seq.augment_image(image)
 
 
 class MapillaryDataset(Dataset):
@@ -100,13 +136,13 @@ class MapillaryDataset(Dataset):
     def __len__(self):
         """Returns the number of items in the dataset.
 
-        This assumes that the number of images in the images directory is equal
-        to the number of image in every on eof the other directories.
+        Calculates by getting a len of the self.images list, which contains all
+        the values found in viable.txt
 
         Returns:
             int: The number of images in the dataset.
         """
-        return len(listdir(self.seg_dir))
+        return len(self.images)
 
     def __getitem__(self, idx):
         """Returns the items in the dataset with the specified idx.
@@ -130,9 +166,6 @@ class MapillaryDataset(Dataset):
             "raw": raw,
             "segmented": segmented
         }
-
-        # Resize the images in the dict
-
         return out
 
     def _process_raw(self, image):
@@ -145,13 +178,12 @@ class MapillaryDataset(Dataset):
         Returns:
             torch.Tensor: The image as a torch tensor.
         """
-        # Resize the image to 640 x 480
-        image = ia.imresize_single_image(image, (640, 480),
-                                         interpolation='cubic')
+        # Crop the image to 4:3 ratio
+        image = resize(image, "cubic")
 
         if self.with_aug:
             # apply image augmentation sequential
-            image = Augment.augment(image)
+            image = augment(image)
 
         # swap color axis because
         # numpy image: H x W x C
@@ -175,8 +207,7 @@ class MapillaryDataset(Dataset):
             np.array: An array of the images with one-hot labelling.
         """
         # First resize the image
-        image = ia.imresize_single_image(image, (640, 480),
-                                         interpolation='nearest')
+        image = resize(image, "nearest")
 
         # Create an out array
         out_array = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)

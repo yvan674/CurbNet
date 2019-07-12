d3b85a81c03ae8bfbbc01d4bde91dc8c9cf0ac1b
y_satyawan@hotmail.com <eRw-X7L-Csp-GdN>
2019-07-12 14:59:31 +0200

    Enabled silent operation

--- src/train.sh
+++ src/train.sh
@@ -3,12 +3,12 @@
 # time.
 
 DATE=$(date +"%d-%m")
-WEIGHTS="/home/satyaway/Documents/Thesis/Weights/with-eps-2.pth"
+WEIGHTS="/home/satyaway/Documents/Thesis/Weights/regularized.pth"
 DATA="/tmp/mapillary-resized/"
 LR=0.0002
 OPTIMIZER="adam"
 BATCH=32
-EPOCH=10
+EPOCH=40
 PLOT="/home/satyaway/Documents/Thesis/Logs"
 NETWORK=d
 PRETRAINED=""
